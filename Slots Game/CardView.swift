d082de6b587ba112053caa2040908f5402e2ce1c
Eugene <contact.duk@gmail.com>
2022-02-20 13:59:57 +0300

    Adding initial source code

---
+++ Slots Game/CardView.swift
@@ -0,0 +1,30 @@
+//
+//  CardView.swift
+//  Slots Game
+//
+//  Created by Eugene on 27.11.21.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    @Binding var symbol:String
+    @Binding var background:Color
+    
+    var body: some View {
+      
+        Image(symbol)
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .background(background.opacity(0.5))
+            .cornerRadius(20)
+        
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardView(symbol: Binding.constant("cherry"), background: Binding.constant(Color.green))
+    }
+}
---
+++ Slots Game/Slots_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_GameApp.swift
+//  Slots Game
+//
+//  Created by Eugene on 26.11.21.
+//
+
+import SwiftUI
+
+@main
+struct Slots_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
